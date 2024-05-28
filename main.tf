07fec17984ad161514b3bac560e0c7bfeae74a93
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-05-28 23:26:46 +0530

    Terraform security

--- main.tf
+++ main.tf
@@ -1,20 +1,18 @@
 resource "aws_security_group" "allow_tls" {
-
-    name = local.sg_name_final
-    description = var.sg_description
-    vpc_id = var.vpc_id
-
-    dynamic "ingress" {
-        
-        for_each = var.ingress_rules
-        content {
-          from_port     =   egress.value["from_port"]
-          to_port       =   egress.value["to_port"]
-          protocol      =   egress.value["protocol"]
-          cidr_blocks   =   egress.value["cidr_blocks"]
-        }
-      
+  name        = local.sg_name_final
+  description = var.sg_description
+  vpc_id      = var.vpc_id
+  
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+        from_port       = egress.value["from_port"]
+        to_port         = egress.value["to_port"]
+        protocol        = egress.value["protocol"]
+        cidr_blocks = egress.value["cidr_blocks"]
     }
+  }
+
 
     dynamic "egress" {
 
